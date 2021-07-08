f77efabccc69ab4a771aedf1e361224b44817c71
A-amon <64846892+A-amon@users.noreply.github.com>
2021-07-08 15:12:10 +0800

    Setup redux store and router

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,21 @@
-import './css/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from '../components/Header/Header'
+import Home from '../pages/Home/Home'
+import './css/App.css'
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+export default function App () {
+    return (
+        <div className="App">
+            <Router basename={process.env.PUBLIC_URL}>
+                <Header />
+                <main>
+                    <Switch>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </main>
+            </Router>
+        </div>
+    )
 }
-
-export default App;
