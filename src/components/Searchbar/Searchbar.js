b4f34b1d2da5263fc18a3fa7cf1f61f35a295f46
A-amon <64846892+A-amon@users.noreply.github.com>
2021-07-09 00:27:34 +0800

    Updated Searchbar component

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,40 +1,73 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router'
 import PropTypes from 'prop-types'
-import SearchImg from '../../assets/icons/search.svg'
+import SearchImgSienna from '../../assets/icons/search--sienna.svg'
+import SearchImgConcrete from '../../assets/icons/search--concrete.svg'
+import CloseImg from '../../assets/icons/close.svg'
+import Location from '../Location/Location'
 import './css/Searchbar.css'
 
 Searchbar.propTypes = {
-    onSearch: PropTypes.func
+    onSearch: PropTypes.func,
+    results: PropTypes.array,
+    initLocation: PropTypes.string
 }
 
-export default function Searchbar ({ onSearch }) {
-    const [location, setLocation] = useState(null)
-    const [guests, setGuests] = useState(null)
+export default function Searchbar ({ onSearch, results, initLocation }) {
+    const history = useHistory()
 
-    const submitHandler = () => {
+    const [isFocus, setIsFocus] = useState(false)
+    const [location, setLocation] = useState('')
+    const [guests, setGuests] = useState('')
+
+    useEffect(() => {
+        setLocation(initLocation)
+    }, [initLocation])
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
         const search = {}
-        if (location !== null) {
+        if (location !== null && location.length > 0) {
             search.location = location
         }
 
-        if (guests !== null) {
+        if (guests !== null && guests.length > 0) {
             search.guests = guests
         }
 
         onSearch(search)
     }
 
+    const resultClicked = (value) => {
+        setLocation(`${value.city}, ${value.country}`)
+        setIsFocus(false)
+        history.push(`/city=${value.city}&country=${value.country}`)
+    }
+
     return (
-        <>
+        <div className={`searchbar ${isFocus ? 'searchbar--focused' : ''}`}>
+            {
+                isFocus
+                    ? <div className="searchbar__header">
+                        <span>Edit your search</span>
+                        <button onClick={e => setIsFocus(false)}>
+                            <img src={CloseImg} alt="Close search" />
+                        </button>
+                    </div>
+                    : null
+            }
             <span className="aria--hide">Search by Location and Guests</span>
-            <form className='searchbar' onSubmit={e => submitHandler()}>
+            <form className='searchbar__form' onSubmit={submitHandler}>
                 <div className="searchbar__group">
                     <label htmlFor="searchLocation">Location</label>
                     <input type="text"
                         id="searchLocation"
                         placeholder="Choose location"
                         value={location}
-                        onChange={e => setLocation(e.target.value)} />
+                        onChange={e => setLocation(e.target.value)}
+                        onFocus={e => setIsFocus(true)}
+                    />
                 </div>
                 <div className="searchbar__group">
                     <label htmlFor="searchGuests">Guests</label>
@@ -42,13 +75,32 @@ export default function Searchbar ({ onSearch }) {
                         id="searchGuests"
                         placeholder="Add guests"
                         value={guests}
-                        onChange={e => setGuests(e.target.value)} />
+                        onChange={e => setGuests(e.target.value)}
+                        onFocus={e => setIsFocus(true)}
+                    />
                 </div>
+                {
+                    isFocus
+                        ? <ul role='listbox' className="searchbar__locations">
+                            {
+                                results.map((result, ind) => <Location key={ind}
+                                    role='listitem'
+                                    city={result.city}
+                                    country={result.country}
+                                    onClick={resultClicked} />)
+                            }
+                        </ul>
+                        : null
+                }
                 <button className="searchbar__submit" type="submit">
-                    <img src={SearchImg} alt="" />
+                    {
+                        isFocus
+                            ? <img src={SearchImgConcrete} alt="" />
+                            : <img src={SearchImgSienna} alt="" />
+                    }
                     <span>Search</span>
                 </button>
             </form>
-        </>
+        </div>
     )
 }
